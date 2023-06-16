8fca7cd7a95938d70ba44fc92ce57c9a5af64847
Henry Tian <43108245+LifeIsHardBruh@users.noreply.github.com>
2023-06-16 12:45:35 -0400

    Add files via upload

---
+++ models/reviewModel.js
@@ -0,0 +1,19 @@
+import pool from "./db.js";
+
+class reviewModel {
+    static getReviewByRid(rid) {
+    }
+
+    static updateReviewTextByRid(rid) {
+
+    }
+
+    static deleteReviewByRid(rid) {
+
+    }
+
+    // emoji: 1: useful, 2: funny, 3: cool
+    static emojiByRid(rid, emoji) {
+
+    }
+}
