085c1ad38dc3783ffe6f710059eec25b87348eff
IIGZYII <iigzyii@hotmail.com>
2023-06-21 17:39:52 -0400

    update

--- SQL-query/CreateUserFile.sql
+++ SQL-query/CreateUserFile.sql
@@ -2,7 +2,7 @@ CREATE TABLE UserFile (
 	uid varchar(255),
 	name varchar(255),
 	PRIMARY KEY(uid, name),
-    FOREIGN KEY(uid) REFERENCES UserAuth(uid)
+    FOREIGN KEY(uid) REFERENCES UserAuth(uid) ON DELETE CASCADE
 );
 
 CREATE TRIGGER addUser
