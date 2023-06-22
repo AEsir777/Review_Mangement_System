9a3c3d472e3cd253ee2bcbb62a17d3a45fc275b6
AEsir777 <lkb2438834817@gmail.com>
2023-06-21 22:24:09 -0400

    push

--- SQL-query/CreateFriend.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Friend (
-	uid1 varchar(36) NOT NULL,
-	uid2 varchar(36) NOT NULL,
-	PRIMARY KEY(uid1, uid2),
-	FOREIGN KEY(uid1) REFERENCES UserFile(uid) ON DELETE CASCADE,
-	FOREIGN KEY(uid2) REFERENCES UserFile(uid) ON DELETE CASCADE
-);
