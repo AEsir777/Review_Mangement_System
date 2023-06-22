4319a4baa7ad3f496012e69bd4ab664e2ffbab06
IIGZYII <iigzyii@hotmail.com>
2023-06-21 23:49:20 -0400

    create

--- SQL-query/create-table-trigger/CreateCoolTriggers.sql
+++ SQL-query/create-table-trigger/CreateCoolTriggers.sql
@@ -2,14 +2,12 @@ CREATE TRIGGER addCool
 AFTER INSERT ON coolHistory
 FOR EACH ROW
     UPDATE review
-    SET reviewCount = reviewCount + 1,
-		cool = cool + 1
+    SET cool = cool + 1
     WHERE review.rid = NEW.rid
 
 CREATE TRIGGER subCool
 BEFORE DELETE ON coolHistory
 FOR EACH ROW
     UPDATE review
-    SET reviewCount = reviewCount - 1,
-		cool = cool + 1
+    SET cool = cool - 1
     WHERE review.rid = OLD.rid
--- SQL-query/create-table-trigger/CreateUserFile.sql
+++ SQL-query/create-table-trigger/CreateUserFile.sql
@@ -13,3 +13,17 @@ AFTER INSERT ON UserAuth
 FOR EACH ROW
 	INSERT INTO UserFile(uid, name, createTime, cools) 
     VALUES(NEW.uid, NEW.email, NOW(), 0, 0);
+
+CREATE TRIGGER addCools
+AFTER INSERT ON coolHistory
+FOR EACH ROW
+    UPDATE UserFile
+    SET cool = cool + 1
+    WHERE review.rid = NEW.rid
+
+CREATE TRIGGER subCools
+BEFORE DELETE ON coolHistory
+FOR EACH ROW
+    UPDATE review
+    SET cool = cool - 1
+    WHERE review.rid = OLD.rid
