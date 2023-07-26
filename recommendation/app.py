88fa4005063d4075476ea7d46c6257ac2b7a1ae3
Luc1ferxx <jixuxiao1@gmail.com>
2023-07-26 14:20:14 -0400

    updated app

--- recommendation/app.py
+++ recommendation/app.py
@@ -2,41 +2,28 @@ import pandas as pd
 import numpy as np
 from flask import Flask, request, jsonify
 from surprise import Reader, Dataset, SVD
-from surprise.model_selection import cross_validate, train_test_split
-from surprise import accuracy
+from surprise.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 from datetime import datetime
+import pickle
 import nltk
 nltk.download('vader_lexicon')
 from nltk.sentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 
-def recommend_businesses(uid):
+def train_and_save_model():
     # Load the data into pandas dataframes
     review_df = pd.read_csv('Review.csv')
     review_with_df = pd.read_csv('ReviewWith.csv')
     cate_df = pd.read_csv('Cate.csv')
-    user_file_df = pd.read_csv('UserFile.csv')
-    friend_df = pd.read_csv('Friend.csv')
-    cool_history_df = pd.read_csv('coolHistory.csv')
 
     # Time-Based Filtering: Add weight to recent reviews
     current_year = datetime.now().year
     review_df['date'] = pd.to_datetime(review_df['date'])
     review_df['year'] = review_df['date'].dt.year
     review_df['recency_weight'] = review_df['year'].apply(lambda x: current_year - x + 1)
-
-    # Incorporate UserFile and UserAuth data: Weight users based on how long they've been on the platform
-    user_file_df['createTime'] = pd.to_datetime(user_file_df['createTime'])
-    user_file_df['account_age'] = user_file_df['createTime'].apply(lambda x: current_year - x.year + 1)
-
-    # Social Network-Based Recommendations: Recommend businesses rated highly by friends
-    friends = set(friend_df[friend_df['uid1'] == uid]['uid2'])
-
-    # Use coolHistory data: Weight reviews by users who have a lot of 'cool' ratings
-    cool_users = set(cool_history_df['uid'])
 
     # Sentiment Analysis on Reviews: Add sentiment score to the reviews
     sia = SentimentIntensityAnalyzer()
@@ -52,7 +39,6 @@ def recommend_businesses(uid):
     business_cate_df['cate'] = business_cate_df['cate'].fillna('')
     business_to_categories = business_cate_df.groupby('bid')['cate'].apply(lambda x: ', '.join(x)).to_dict()
 
-
     # Create a TfidfVectorizer object
     vectorizer = TfidfVectorizer(token_pattern=r'[^\s]+')
 
@@ -61,9 +47,6 @@ def recommend_businesses(uid):
 
     # Compute the cosine similarity matrix
     cos_sim = cosine_similarity(X, X)
-
-    # Create a reverse map of indices and business ids
-    indices = pd.Series(range(len(business_to_categories)), index=business_to_categories.keys())
 
     # Define a Reader object
     reader = Reader(rating_scale=(1, 5))
@@ -80,11 +63,56 @@ def recommend_businesses(uid):
     # Train the algorithm with the train_set
     algo.fit(train_set)
 
-    # Predict ratings for the test_set
-    predictions = algo.test(test_set)
+    # Save the trained model and necessary data using pickle
+    with open('trained_model.pkl', 'wb') as f:
+        pickle.dump(algo, f)
+    with open('cosine_similarity_matrix.pkl', 'wb') as f:
+        pickle.dump(cos_sim, f)
+    with open('tfidf_vectorizer.pkl', 'wb') as f:
+        pickle.dump(vectorizer, f)
 
-    # Then compute RMSE
-    accuracy.rmse(predictions)
+def load_pretrained_model():
+    # Load the trained SVD model
+    with open('trained_model.pkl', 'rb') as f:
+        algo = pickle.load(f)
+
+    # Load the cosine similarity matrix
+    with open('cosine_similarity_matrix.pkl', 'rb') as f:
+        cos_sim = pickle.load(f)
+
+    # Load the TfidfVectorizer
+    with open('tfidf_vectorizer.pkl', 'rb') as f:
+        vectorizer = pickle.load(f)
+
+    return algo, cos_sim, vectorizer
+
+def recommend_businesses(uid, algo, cos_sim, vectorizer):
+    # Load the data into pandas dataframes
+    review_df = pd.read_csv('Review.csv')
+    review_with_df = pd.read_csv('ReviewWith.csv')
+    cate_df = pd.read_csv('Cate.csv')
+
+    # Join the two dataframes on the review id
+    joined_df = pd.merge(review_df, review_with_df, how='inner', on='rid')
+
+    # Create a merged dataframe of businesses and their categories
+    business_cate_df = pd.merge(joined_df, cate_df, how='inner', on='bid')
+
+    # Preprocess categories: create a dict where each business_id is associated with a string of its categories
+    business_cate_df['cate'] = business_cate_df['cate'].fillna('')
+    business_to_categories = business_cate_df.groupby('bid')['cate'].apply(lambda x: ', '.join(x)).to_dict()
+
+    # Create a TfidfVectorizer object (use the provided vectorizer)
+    vectorizer = vectorizer
+
+    # Fit and transform the cate column (use the provided vectorizer)
+    X = vectorizer.transform(business_to_categories.values())
+
+    # Compute the cosine similarity matrix (use the provided cos_sim)
+    cos_sim = cos_sim
+
+    # Create a reverse map of indices and business ids
+    indices = pd.Series(range(len(business_to_categories)), index=business_to_categories.keys())
 
     # Get the list of all business ids
     all_business_ids = set(joined_df['bid'])
@@ -117,8 +145,13 @@ def recommend_businesses(uid):
 @app.route('/recommend', methods=['GET'])
 def recommend():
     uid = request.args.get('uid')
-    recommendations = recommend_businesses(uid)
+    algo, cos_sim, vectorizer = load_pretrained_model()
+    recommendations = recommend_businesses(uid, algo, cos_sim, vectorizer)
     return jsonify(recommendations)
 
 if __name__ == '__main__':
+    # Train and save the model before running the Flask app
+    train_and_save_model()
+
+    # Start the Flask app
     app.run()
