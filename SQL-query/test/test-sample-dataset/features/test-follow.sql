feeb02accfcde726892e785d6910e32c61153f16
Henry Tian <43108245+LifeIsHardBruh@users.noreply.github.com>
2023-07-27 19:35:41 -0400

    update sample dataset tests

--- SQL-query/test/test-sample-dataset/features/test-follow.sql
+++ SQL-query/test/test-sample-dataset/features/test-follow.sql
@@ -1,16 +1,25 @@
 /**** add friend ******/
 /*** Test insert and isFriend ******/
--- check: not exist before
-CALL CheckFriendship('U3', 'U8');
+-- check
+CALL GetFollowers('U3'); -- the result shouldn't include U8
+CALL GetFollowers('U8'); -- the result shouldn't include U3
+CALL GetFollowings('U3'); -- the result shouldn't include U8
+CALL GetFollowings('U8'); -- the result shouldn't include U3
 -- success case
 CALL InsertFriend('U3', 'U8');
--- check
-CALL CheckFriendship('U3', 'U8');
+CALL InsertFriend('U8', 'U3');
+-- -- check
+CALL GetFollowers('U3'); -- the result now include U8
+CALL GetFollowers('U8'); -- the result now include U3
+CALL GetFollowings('U3'); -- the result now include U8
+CALL GetFollowings('U8'); -- the result now include U3
 
 -- check: exist before
-CALL CheckFriendship('U1', 'U2');
-CALL CheckFriendship('U7', 'U8');
-CALL CheckFriendship('U1', 'U6');
+CALL GetFollowers('U1');
+CALL GetFollowers('U2');
+CALL GetFollowings('U1');
+CALL GetFollowings('U2');
+
 
 -- check: insert fail
 CALL InsertFriend('U1', 'U2');
