a604b628045b8a6c7b4af5031774e58e5849db26
AEsir777 <lkb2438834817@gmail.com>
2023-07-27 18:28:05 -0400

    fix test case

--- SQL-query/test/test-production-dataset/test-authentication.sql
+++ SQL-query/test/test-production-dataset/test-authentication.sql
@@ -6,8 +6,8 @@ CALL InsertIntoUserAuth('123e4567-e89b-12d3-a456-426614171234', 'test2@example.c
 /*** Test get user ***/
 CALL GetUserByUid('123e4567-e89b-12d3-a456-426614174000');
 CALL GetUserByUid('123e4567-e89b-12d3-a456-426614171234');
-CALL GetUserByUid('"w-zD8Ln3XZszM82AfVrspg"');
-CALL GetUserByUid('"qvmL6mp-rpIwVguxb5HuvQ"');
-CALL GetUserByUid('"eTvp_hYnsrI5-ow_sQ31_g"');
-CALL GetUserByUid('"GG0mFsEXb-02_dzFPqRV1Q"');
-CALL GetUserByUid('"not exist"');
+CALL GetUserByUid('w-zD8Ln3XZszM82AfVrspg');
+CALL GetUserByUid('qvmL6mp-rpIwVguxb5HuvQ');
+CALL GetUserByUid('eTvp_hYnsrI5-ow_sQ31_g');
+CALL GetUserByUid('GG0mFsEXb-02_dzFPqRV1Q');
+CALL GetUserByUid('not exist');
