ee97906b78e34e89e348db6811a0f38913b8cdcc
Henry Tian <43108245+LifeIsHardBruh@users.noreply.github.com>
2023-07-27 21:03:11 -0400

    update production tests

--- SQL-query/test/test-production-dataset/test-follow.sql
+++ SQL-query/test/test-production-dataset/test-follow.sql
@@ -1,28 +1,44 @@
 /**** add friend ******/
 /*** Test insert and isFriend ******/
 -- check: not exist before
-CALL CheckFriendship("rppTTi-kfF8-qyiArNemag", "RgDVC3ZUBqpEe6Y1kPhIpw");
+CALL GetFollowers('_3R2OM6fTkdcc3rPGTZYAQ'); -- the result shouldn't include '_4gdJdZFM0EnWr6v1Qr3A'
+CALL GetFollowers('_4gdJdZFM0EnWr6v1Qr3A'); -- the result shouldn't include '_3R2OM6fTkdcc3rPGTZYAQ'
+CALL GetFollowings('_3R2OM6fTkdcc3rPGTZYAQ'); -- the result shouldn't include '_4gdJdZFM0EnWr6v1Qr3A'
+CALL GetFollowings('_4gdJdZFM0EnWr6v1Qr3A'); -- the result shouldn't include '_3R2OM6fTkdcc3rPGTZYAQ'
 -- success case
-CALL InsertFriend("rppTTi-kfF8-qyiArNemag", "RgDVC3ZUBqpEe6Y1kPhIpw");
+CALL InsertFriend('_4gdJdZFM0EnWr6v1Qr3A', '_3R2OM6fTkdcc3rPGTZYAQ');
+CALL InsertFriend('_3R2OM6fTkdcc3rPGTZYAQ', '_4gdJdZFM0EnWr6v1Qr3A');
 -- check
-CALL CheckFriendship("rppTTi-kfF8-qyiArNemag", "RgDVC3ZUBqpEe6Y1kPhIpw");
+CALL GetFollowers('_3R2OM6fTkdcc3rPGTZYAQ'); 
+CALL GetFollowers('_4gdJdZFM0EnWr6v1Qr3A'); 
+CALL GetFollowings('_3R2OM6fTkdcc3rPGTZYAQ');
+CALL GetFollowings('_4gdJdZFM0EnWr6v1Qr3A'); 
 
-CALL CheckFriendship("rppTTi-kfF8-qyiArNemag", "sHozd2pcOKwHtPr8VlZJfg");
+CALL GetFollowers('_3R2OM6fTkdcc3rPGTZYAQ'); 
+CALL GetFollowers('0rb0x3YydlvYdbFUysfsg');
+CALL GetFollowings('_3R2OM6fTkdcc3rPGTZYAQ');
+CALL GetFollowings('0rb0x3YydlvYdbFUysfsg');
+
 -- success case 
-CALL InsertFriend("rppTTi-kfF8-qyiArNemag", "sHozd2pcOKwHtPr8VlZJfg");
+CALL InsertFriend("0rb0x3YydlvYdbFUysfsg", "_3R2OM6fTkdcc3rPGTZYAQ");
+CALL InsertFriend("_3R2OM6fTkdcc3rPGTZYAQ", "0rb0x3YydlvYdbFUysfsg");
+
 -- check
-CALL CheckFriendship("rppTTi-kfF8-qyiArNemag", "sHozd2pcOKwHtPr8VlZJfg");
+CALL GetFollowers('_3R2OM6fTkdcc3rPGTZYAQ'); 
+CALL GetFollowers('0rb0x3YydlvYdbFUysfsg');
+CALL GetFollowings('_3R2OM6fTkdcc3rPGTZYAQ');
+CALL GetFollowings('0rb0x3YydlvYdbFUysfsg');
 
 -- check: exist before
-CALL CheckFriendship("LwZJFLGxQwjjeOgpqTJnfw", "RHsmgIvCfnFL7DPNptKY8A");
-CALL CheckFriendship("c7XI431TnC9_AkmC-qrXvQ", "RgDVC3ZUBqpEe6Y1kPhIpw");
-CALL CheckFriendship("w-zD8Ln3XZszM82AfVrspg", "RgDVC3ZUBqpEe6Y1kPhIpw");
-CALL CheckFriendship("8Akq8sh1LSmJbqa6Jjxxqg", "96IKLbNHdK-sOAjuMwoegw");
-CALL CheckFriendship("TDXV6AC5PYOZEyN9eeODfA", "7xp_8X8QRAgHFZGs8M7FCw");
-CALL CheckFriendship("UVUIi0q38pAvsfuEEppe0Q", "JFIr4bbO2zjQYdA3LwbbsQ");
-CALL CheckFriendship("aA0WQpDdksKuDafTGzJf_A", "qvmL6mp-rpIwVguxb5HuvQ");
-CALL CheckFriendship("q9xZ_o95MTmBmLLPnZyBwQ", "47iiqGmXi6UtE0wd8bRWpQ");
-CALL CheckFriendship("Js4nQlGRjS1Bd5vsAQQfgA", "4YwesZEX8VPPJOKLfPD7lg");
+CALL GetFollowers("_GvKig8yKIp_WtFtWpntkg");
+CALL GetFollowings("qms0w0pmjjjaeNojF3qxQ");
+CALL GetFollowers("_H6E5kheFYwu52kCZlF8qg"); 
+CALL GetFollowings("1lmyWoPFb_eJvG5Zw3UA");
+CALL GetFollowers("_iYyZtuWMmEMFCOKRLYgg"); 
+CALL GetFollowings("pQrWjL22uvSWMwS9yZ_Yg");
+CALL GetFollowers("_KeuaQCbJsZ7cipYpBPxDQ"); 
+CALL GetFollowings("coWYR6JBcD9Qa55ibyqr9Q");
+CALL GetFollowers("V3XYG1zKuW8VUBNtjacoxA");
 
 -- check: insert fail
 CALL InsertFriend("LwZJFLGxQwjjeOgpqTJnfw", "RHsmgIvCfnFL7DPNptKY8A");
